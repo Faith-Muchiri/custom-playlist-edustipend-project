772495354328e586506a27e24736da9b9ebe60d8
Faith <faithwambui891@gmail.com>
2023-05-19 23:16:30 +0300

    modify the import

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../components/navbar/Navbar';
+import MainSection from '../components/MainSection';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <MainSection />
+    </div>
+  );
+};
+
+export default HomePage;
