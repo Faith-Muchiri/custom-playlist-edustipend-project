6ed0dd6404a7b2bd6490b10f5fecd596ea87c3df
Faith <faithwambui891@gmail.com>
2023-05-19 09:01:13 +0300

    create navbad and add styling

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <span className='navbar__text'>Contact us</span>
+      <button className='signup__btn'>Signup</button>
+    </nav>
+  );
+};
+
+export default Navbar;
